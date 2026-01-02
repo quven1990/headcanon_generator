09cb8088d9a8f7d3086288e190100739b93acdf6
xueh3 <xueh3@ke.com>
2026-01-03 00:04:18 +0800

    添加新包

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata, Viewport } from "next"
 import Script from "next/script"
 import { Geist, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Navbar } from "@/components/navbar"
 import "./globals.css"
 
@@ -82,6 +83,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
